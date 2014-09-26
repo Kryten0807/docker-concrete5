cc4f638bdd6769d38761c7701fe02b7258ec0198
Dave Thomas <dthomas@starkartthenala.com>
2014-09-26 07:40:50 -0700

    replaced hard-coded database username with $DB_USER

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-if [ -f /.mysql_db_created ]; 
+if [ -f /.mysql_db_created ];
 then
         exec supervisord -n
         exit 1
 fi
 
 sleep 5
-DB_EXISTS=$(mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE 'concrete5';" | grep "concrete5" > /dev/null; echo "$?")
+DB_EXISTS=$(mysql -u$DB_USER -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE 'concrete5';" | grep "concrete5" > /dev/null; echo "$?")
 
-if [[ DB_EXISTS -eq 1 ]]; 
+if [[ DB_EXISTS -eq 1 ]];
 then
         echo "=> Creating database concrete5"
         RET=1
         while [[ RET -ne 0 ]]; do
                 sleep 5
-                mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "CREATE DATABASE $DB_NAME"
+                mysql -u$DB_USER -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "CREATE DATABASE $DB_NAME"
                 RET=$?
         done
         echo "=> Done!"
